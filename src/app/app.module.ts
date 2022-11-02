933bab05173cf5dad0c0b5917fdb4bfe3d5bcb7d
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-11-02 15:13:59 +0100

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,70 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+
+// import { RouterModule, Routes } from '@angular/router';
+// import { MatIconModule } from '@angular/material/icon';
+// import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+// import { MovieCardComponent } from './movie-card/movie-card.component';
+// import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+// import { ProfileComponent } from './profile/profile.component';
+// import { EditProfileComponent } from './edit-profile/edit-profile.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+// import { DirectorComponent } from './director/director.component';
+// import { GenreComponent } from './genre/genre.component';
+// import { SynopsisComponent } from './synopsis/synopsis.component';
+
+// const appRoutes: Routes = [
+//   { path: 'welcome', component: WelcomePageComponent },
+//   { path: 'movies', component: MovieCardComponent },
+//   { path: 'profile', component: ProfileComponent },
+//   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+// ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationFormComponent,
+    UserLoginFormComponent,
+    // UserLoginFormComponent,
+    // MovieCardComponent,
+    // WelcomePageComponent,
+    // ProfileComponent,
+    // EditProfileComponent,
+    // NavbarComponent,
+    // DirectorComponent,
+    // GenreComponent,
+    // SynopsisComponent
   ],
+
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    HttpClientModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    // RouterModule.forRoot(appRoutes),    
+    // MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,53 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+// You'll use this import to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// This import brings in the API calls we created in 6.2
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss'],
+})
+export class UserLoginFormComponent implements OnInit {
+  @Input() userCredentials = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void { }
+
+  /**
+ * sends form inputs for user login to backend via fetchApiData Service
+ */
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userCredentials).subscribe(
+      (result) => {
+        console.log(result);
+        // Add token and username to local Storage
+        localStorage.setItem('token', result.token);
+        localStorage.setItem('user', result.user.Username);
+        this.dialogRef.close(); // Close the modal on success
+        this.router.navigate(['movies']);
+      },
+      (result) => {
+        console.log(result);
+        this.snackBar.open(result, 'OK', {
+          duration: 2000,
+        });
+      }
+    );
+  }
+}
