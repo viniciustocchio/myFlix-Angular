d181c980fed740eba028d34f7f0f9dc884c54839
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-11-05 12:42:49 +0100

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,6 +9,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,39 +17,36 @@ import { FormsModule } from '@angular/forms';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { ProfileComponent } from './profile/profile.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
-// import { RouterModule, Routes } from '@angular/router';
-// import { MatIconModule } from '@angular/material/icon';
-// import { MovieCardComponent } from './movie-card/movie-card.component';
-// import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-// import { ProfileComponent } from './profile/profile.component';
-// import { EditProfileComponent } from './edit-profile/edit-profile.component';
-// import { NavbarComponent } from './navbar/navbar.component';
-// import { DirectorComponent } from './director/director.component';
-// import { GenreComponent } from './genre/genre.component';
-// import { SynopsisComponent } from './synopsis/synopsis.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DirectorComponent } from './director/director.component';
+import { GenreComponent } from './genre/genre.component';
+import { SynopsisComponent } from './synopsis/synopsis.component';
 
-// const appRoutes: Routes = [
-//   { path: 'welcome', component: WelcomePageComponent },
-//   { path: 'movies', component: MovieCardComponent },
-//   { path: 'profile', component: ProfileComponent },
-//   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
-// ];
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
     UserLoginFormComponent,
-    // UserLoginFormComponent,
-    // MovieCardComponent,
-    // WelcomePageComponent,
-    // ProfileComponent,
-    // EditProfileComponent,
-    // NavbarComponent,
-    // DirectorComponent,
-    // GenreComponent,
-    // SynopsisComponent
+    MovieCardComponent,
+    WelcomePageComponent,
+    ProfileComponent,
+    EditProfileComponent,
+    NavbarComponent,
+    DirectorComponent,
+    GenreComponent,
+    SynopsisComponent,
   ],
 
   imports: [
@@ -62,10 +61,11 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
     MatCardModule,
     MatFormFieldModule,
     MatSnackBarModule,
-    // RouterModule.forRoot(appRoutes),    
-    // MatIconModule
+    RouterModule.forRoot(appRoutes),    
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
