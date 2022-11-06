b210c16355700a854dc4d9bd75d2edfc96285118
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-11-06 10:25:20 +0100

    added style and fixed movie card component

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -15,13 +15,76 @@ export class MovieCardComponent implements OnInit {
 
   ngOnInit(): void {
     this.getMovies();
+    this.getFavoriteMovies();
   }
-
+    
   getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
-        this.movies = resp;
-        console.log(this.movies);
-        return this.movies;
-      });
+    this.movies = resp;
+    console.log(this.movies);
+    return this.movies;
+    });
+  }
+  
+  getFavoriteMovies(): void {
+    this.fetchApiData.getFavoriteMovies().subscribe((resp: any) => {
+    this.favoriteMovies = resp;
+    console.log(this.favoriteMovies);
+    return this.favoriteMovies;
+    });
+  }
+  
+  isFav(id: string): boolean {
+    return this.favoriteMovies.includes(id);
+  }
+  
+  openGenreDialog(name: string, description: string): void {
+    this.dialog.open(GenreComponent, {
+    data: {
+    Name: name,
+    Description: description,
+    },
+    // Assign dialog width
+    width: '500px',
+    });
+  }
+  
+  openDirectorDialog(name: string, bio: string, birthday: string): void {
+    this.dialog.open(DirectorComponent, {
+    data: {
+    Name: name,
+    Bio: bio,
+    Birth: birthday,
+    },
+    // Assign dialog width
+    width: '500px',
+    });
+  }
+  
+  openSynopsisDialog(title: string, description: string): void {
+    this.dialog.open(SynopsisComponent, {
+    data: {
+    Title: title,
+    Description: description,
+    },
+    // Assign dialog width
+    width: '500px',
+    });
+  }
+  
+  addToFavoriteMovies(id: string): void {
+    console.log(id);
+    this.fetchApiData.addFavoriteMovie(id).subscribe((result) => {
+    console.log(result);
+    this.ngOnInit();
+    });
+  }
+  
+  removeFromFavoriteMovies(id: string): void {
+    console.log(id);
+    this.fetchApiData.removeFavoriteMovie(id).subscribe((result) => {
+    console.log(result);
+    this.ngOnInit();
+    });
   }
 }
--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MovieCardComponent } from '../movie-card/movie-card.component';
 
 @Component({
   selector: 'app-welcome-page',
@@ -21,5 +22,10 @@ export class WelcomePageComponent implements OnInit {
     this.dialog.open(UserLoginFormComponent, {
       width: '280px'
     });
+  }
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
+    }); 
   }
 }
